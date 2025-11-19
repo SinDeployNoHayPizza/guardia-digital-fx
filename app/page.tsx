cb7a99ca0cc5d411443fc5b50ba35dbc136bc268
Eduardo C. <emcdesarrollos@gmail.com>
2025-11-19 00:42:58 -0300

    feat: Added react lenis and basic smoth scroll effect

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,12 @@ import Hero from "./components/home/Hero";
 import ServicesGrid from "./components/home/ServicesGrid";
 import Testimonials from "./components/home/Testimonials";
 
+import { ReactLenis } from 'lenis/react'
+
 export default function Home() {
   return (
     <MainLayout>
+      <ReactLenis />
       <Hero />
       <ServicesGrid />
       <Testimonials />
